570e726e15daf3048fe958adf622796198306e2d
Methusela Fernando <136374272+methusela001@users.noreply.github.com>
2023-09-11 22:25:45 +0200

    Update CalculatorTest.java

--- src/main/java/org/example/CalculatorTest.java
+++ src/main/java/org/example/CalculatorTest.java
@@ -1,2 +1,29 @@
-package org.example;public class CalculatorTest {
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathOperationsTest {
+
+    public void testSubtractionPositiveNumbers() {
+        int result = MathOperations.subtraction(10, 5);
+        assertEquals(5, result, "Subtraction of 10 and 5 should be 5");
+    }
+
+    @Test
+    public void testSubtractionNegativeNumbers() {
+        int result = MathOperations.subtraction(-8, -3);
+        assertEquals(-5, result, "Subtraction of -8 and -3 should be -5");
+    }
+
+    @Test
+    public void testMultiplicationPositiveNumbers() {
+        int result = MathOperations.multiplication(6, 4);
+        assertEquals(24, result, "Multiplication of 6 and 4 should be 24");
+    }
+
+    @Test
+    public void testMultiplicationZero() {
+        int result = MathOperations.multiplication(0, 100);
+        assertEquals(0, result, "Multiplication by 0 should result in 0");
+    }
 }
